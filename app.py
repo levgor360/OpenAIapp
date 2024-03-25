8c0b3478fdca1dc63c842b1a277c8267493d7cd2
levgor360 <136678959+levgor360@users.noreply.github.com>
2024-03-25 13:50:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import streamlit as st
 from openai import OpenAI
 
 
+
 # Sidebar setup
 with st.sidebar:
     # Title displayed on the side bar
@@ -10,8 +11,8 @@ with st.sidebar:
     # Request OpenAI API key
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     # Check that the key provided starts with sk and has 40 characters
-    if not (openai_api_key.startswith('sk')):
-        st.warning('Invalid API key', icon='⚠️')
+    if not ((openai_api_key.startswith('sk')) and len(openai_api_key) == 51):
+        st.warning('Enter a valid API key', icon='⚠️')
     else:
         st.success('Proceed to entering your prompt message!', icon='👉')
 
