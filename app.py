7a264b4257cdd9d669b7d14245f4a99e760e5cec
levgor360 <136678959+levgor360@users.noreply.github.com>
2024-03-31 19:59:10 +0300

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ if selected_model == 'GPT-3.5':
     chosen_model = "gpt-3.5-turbo"
 elif selected_model == 'GPT-4':
     chosen_model = "gpt-4"
-chosen_temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=1, step=0.01)
+chosen_temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=1.0, step=0.01)
 chosen_top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=1.0, step=0.01)
 chosen_max_length = st.sidebar.slider('max_length', min_value=32.0, max_value=10000.0, value=500.0, step=8.0)
 chosen_number_of_samples = st.sidebar.slider('Number of samples', min_value=1, max_value=3, value=1, step=1)
