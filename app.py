45f020ef0a93c8b78e5c90f42b84df467e26316c
levgor360 <136678959+levgor360@users.noreply.github.com>
2024-05-12 13:07:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from openai import OpenAI
 with st.sidebar:
     st.title('Enter your model parameters here')     # Title displayed on the side bar
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")     # Request OpenAI API key
-    if not ((openai_api_key.startswith('sk')) and len(openai_api_key) == 51): # Check that the key provided starts with sk and has 40 characters
+    if not ((openai_api_key.startswith('sk')) and len(openai_api_key) == 56): # Check that the key provided starts with sk and has 40 characters
         st.warning('Enter a valid API key', icon='⚠️')
     else:
         st.success('Proceed to entering your prompt message!', icon='👉')
